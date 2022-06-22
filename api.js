b8da708def31b3cd16364ee6afbbf4e1764c6b9f
Kelvin Tony <69513562+kelvintony@users.noreply.github.com>
2022-06-22 11:26:03 +0100

    Add files via upload
    api code

---
+++ api.js
@@ -0,0 +1,15 @@
+export const api = {
+	fetchTrending: `/trending/all/week?api_key=7a107780a1b571645808b28b67008035&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=7a107780a1b571645808b28b67008035&with_networks=213`,
+	fetchTopRated: `/movie/top_ra7a107780a1b571645808b28b67008035&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=7a107780a1b571645808b28b67008035&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=7a107780a1b571645808b28b67008035&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=7a107780a1b571645808b28b67008035&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=7a107780a1b571645808b28b67008035&with_genres=10749`,
+	fetchDocumentries: `/discover/movie?api_key=7a107780a1b571645808b28b67008035&with_genres=99`
+};
+
+export const api_key = {
+	react_api_Url: 'https://api.themoviedb.org/3',
+	react_api_key: '7a107780a1b571645808b28b67008035'
+};
